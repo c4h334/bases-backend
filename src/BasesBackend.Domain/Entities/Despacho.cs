b952b02495bd397a0469500d0466b2373c337882
anderjma <anderson.monge@ucr.ac.cr>
2026-06-08 08:38:14 -0600

    Agregar data anotations faltantes al despacho.cs

--- src/BasesBackend.Domain/Entities/Despacho.cs
+++ src/BasesBackend.Domain/Entities/Despacho.cs
@@ -8,15 +8,22 @@ namespace BasesBackend.Domain.Entities
     public class Despacho
     {
         [Key]
+        [Column("IdDespacho")]
         public int IdDespacho { get; set; }
 
+        [Column("IdCliente")]
         public int IdCliente { get; set; }
 
         [ForeignKey("IdCliente")]
-        public virtual Cliente? Cliente { get; set; }
+        public Cliente? Cliente { get; set; }
 
+        [Column("FechaDespacho")]
         public DateTime FechaDespacho { get; set; }
-        public string Estado { get; set; }
-        public string Operario { get; set; }
+
+        [Column("Estado")]
+        public string Estado { get; set; } = string.Empty;
+
+        [Column("Operario")]
+        public string Operario { get; set; } = string.Empty;
     }
 }
